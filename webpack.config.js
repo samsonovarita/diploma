3341c4d5710746007be488e4ab2fad2a7318e6d7
Margarita Samsonova <ritiksamsonova@yandex.ru>
2019-10-07 18:06:50 +0300

    Webpack is added

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const webpack = require('webpack');
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = { // module.exports — это синтаксис экспорта в Node.js
+    entry: { main: './src/index.js' }, // указали первое место куда заглянет webpack — файл index.js в папке src
+    output: { // указали в какой файл будет собирться весь js и дали ему имя
+        path: path.resolve(__dirname, 'dist'), // переписали точку выхода, используя утилиту path
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{ // тут описываются правила
+                test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+                use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+                exclude: /node_modules/ // исключает папку node_modules
+            },
+            {
+                test: /\.css$/, // применять это правило только к CSS-файлам
+                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|png|jpe?g|gif|svg)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
+                    }
+                },
+                {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        bypassOnDebug: true,
+                        disable: true,
+                    }
+                }]
+            }
+        ]
+    },
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            // Означает, что:
+            inject: false, // стили НЕ нужно прописывать внутри тегов
+            hash: true, // для страницы нужно считать хеш
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+        }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]    
+}
