ff406a8d7645f84667811a5483245196f94fda20
Margarita Samsonova <ritiksamsonova@yandex.ru>
2019-12-08 19:45:55 +0300

    Fix customized input validation

--- src/jscode/analysis.js
+++ src/jscode/analysis.js
@@ -1,27 +1,103 @@
 import "./../css/analysis.css";
+import { apiNews, today, previousWeek } from "./modules/constants.js";
+
+const month = ['ЯНВАРЬ', 'ФЕВРАЛЬ', 'МАРТ', 'АПРЕЛЬ', 'МАЙ', 'ИЮНЬ', 'ИЮЛЬ', 'АВГУСТ', 'СЕНТЯБРЬ', 'ОКТЯБРЬ', 'НОЯБРЬ', 'ДЕКАБРЬ'];
 
 document.querySelector('.answer__title').textContent = `Вы спросили: «${localStorage.getItem('title').replace(/[\"]/gim, "")}»`
 document.querySelector('.answer__news_total').textContent = localStorage.getItem('total').replace(/[\"]/gim, "");
 document.querySelector('.answer__news_in-title').textContent = localStorage.getItem('titles').replace(/[\"]/gim, "");
+document.querySelector('.diagram__text').textContent = `Дата (${month[new Date().getMonth()]})`;
+
+//новая попытка
+const date = new Date();
+const ago = new Date(date.getTime() - previousWeek);
+const week = arrOfDays(ago);
+const data = JSON.parse(localStorage.getItem('data'));
+console.log(data.articles);
+// Массив дат полученных из API
+const dayOfArticle = data.articles.map((el) => { return el.publishedAt });
+const renderedDate = dayOfArticle.map((el) => { return new Date(el).getDate() });
+
+// Получение массива чисел дней
+function arrOfDays(a) {
+    let arr = [];
+    for (let i = 0; i < 7; i += 1) {
+        arr.push(new Date(ago.getTime() + today*i).getDate());
+    }
+    return arr
+}
+
+// Количество новостей за каждый из дней
+function newsInDay(n) {
+    return renderedDate.filter((el) => {
+        return el == n
+    }).length
+}
+
+// День недели 
+function dayOfWeek() {
+    return days.map((el) => { return day[el] })
+}
+
+// Первый день в списке
+document.querySelector('.table__progress_mon').setAttribute('style', `width: ${newsInDay(week[0])}%`);
+document.querySelector('.table__value_mon').textContent = newsInDay(week[0]);
+document.querySelector('.first-day').textContent = `${week[0]}, ${dayOfWeek()[0]}`;
+
+// Второй день в списке
+document.querySelector('.table__progress_tue').setAttribute('style', `width: ${newsInDay(week[1])}%`);
+document.querySelector('.table__value_tue').textContent = newsInDay(week[1]);
+document.querySelector('.second-day').textContent = `${week[1]}, ${dayOfWeek()[1]}`;
+
+// Третий день в списке
+document.querySelector('.table__progress_wed').setAttribute('style', `width: ${newsInDay(week[2])}%`);
+document.querySelector('.table__value_wed').textContent = newsInDay(week[2]);
+document.querySelector('.third-day').textContent = `${week[2]}, ${dayOfWeek()[2]}`;
+
+// Четвертый день в списке
+document.querySelector('.table__progress_thu').setAttribute('style', `width: ${newsInDay(week[3])}%`);
+document.querySelector('.table__value_thu').textContent = newsInDay(week[3]);
+document.querySelector('.fourth-day').textContent = `${week[3]}, ${dayOfWeek()[3]}`;
+
+// Пятый день в списке
+document.querySelector('.table__progress_fri').setAttribute('style', `width: ${newsInDay(week[4])}%`);
+document.querySelector('.table__value_fri').textContent = newsInDay(week[4]);
+document.querySelector('.fifth-day').textContent = `${week[4]}, ${dayOfWeek()[4]}`;
+
+// Шестой день в списке
+document.querySelector('.table__progress_sat').setAttribute('style', `width: ${newsInDay(week[5])}%`);
+document.querySelector('.table__value_sat').textContent = newsInDay(week[5]);
+document.querySelector('.sixth-day').textContent = `${week[5]}, ${dayOfWeek()[5]}`;
+
+// Седьмой день в списке
+document.querySelector('.table__progress_sun').setAttribute('style', `width: ${newsInDay(week[6])}%`);
+document.querySelector('.table__value_sun').textContent = newsInDay(week[6]);
+document.querySelector('.seventh-day').textContent = `${week[6]}, ${dayOfWeek()[6]}`;
+
+//новая попытка
 
 
-document.querySelector('.table__progress_mon').setAttribute('style', `width: ${localStorage.getItem('mon').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_mon').textContent = localStorage.getItem('mon').replace(/[\"]/gim, "");
 
-document.querySelector('.table__progress_tue').setAttribute('style', `width: ${localStorage.getItem('tue').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_tue').textContent = localStorage.getItem('tue').replace(/[\"]/gim, "");
 
-document.querySelector('.table__progress_wed').setAttribute('style', `width: ${localStorage.getItem('wed').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_wed').textContent = localStorage.getItem('wed').replace(/[\"]/gim, "");
 
-document.querySelector('.table__progress_thu').setAttribute('style', `width: ${localStorage.getItem('thu').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_thu').textContent = localStorage.getItem('thu').replace(/[\"]/gim, "");
 
-document.querySelector('.table__progress_fri').setAttribute('style', `width: ${localStorage.getItem('fri').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_fri').textContent = localStorage.getItem('fri').replace(/[\"]/gim, "");
+// document.querySelector('.table__progress_mon').setAttribute('style', `width: ${localStorage.getItem('mon').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_mon').textContent = localStorage.getItem('mon').replace(/[\"]/gim, "");
 
-document.querySelector('.table__progress_sat').setAttribute('style', `width: ${localStorage.getItem('sat').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_sat').textContent = localStorage.getItem('sat').replace(/[\"]/gim, "");
+// document.querySelector('.table__progress_tue').setAttribute('style', `width: ${localStorage.getItem('tue').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_tue').textContent = localStorage.getItem('tue').replace(/[\"]/gim, "");
 
-document.querySelector('.table__progress_sun').setAttribute('style', `width: ${localStorage.getItem('sun').replace(/[\"]/gim, "")}%`);
-document.querySelector('.table__value_sun').textContent = localStorage.getItem('sun').replace(/[\"]/gim, "");
+// document.querySelector('.table__progress_wed').setAttribute('style', `width: ${localStorage.getItem('wed').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_wed').textContent = localStorage.getItem('wed').replace(/[\"]/gim, "");
+
+// document.querySelector('.table__progress_thu').setAttribute('style', `width: ${localStorage.getItem('thu').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_thu').textContent = localStorage.getItem('thu').replace(/[\"]/gim, "");
+
+// document.querySelector('.table__progress_fri').setAttribute('style', `width: ${localStorage.getItem('fri').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_fri').textContent = localStorage.getItem('fri').replace(/[\"]/gim, "");
+
+// document.querySelector('.table__progress_sat').setAttribute('style', `width: ${localStorage.getItem('sat').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_sat').textContent = localStorage.getItem('sat').replace(/[\"]/gim, "");
+
+// document.querySelector('.table__progress_sun').setAttribute('style', `width: ${localStorage.getItem('sun').replace(/[\"]/gim, "")}%`);
+// document.querySelector('.table__value_sun').textContent = localStorage.getItem('sun').replace(/[\"]/gim, "");
--- src/jscode/index.js
+++ src/jscode/index.js
@@ -4,69 +4,93 @@ import { apiNews, today, previousWeek } from "./modules/constants.js";
 import { ResultList } from "./modules/ResultList.js";
 import { makeFormattedDate } from "./modules/changeDate.js";
 
-const search = function (event) {
+const submitButton = document.querySelector('.search__button');
+const searchField = document.querySelector(".search__input");
+
+function activateFormButton(btnElement, state) {
+  if (state) {
+    console.log('Активирую кнопку');
+    btnElement.removeAttribute('disabled');
+    btnElement.classList.add('search__button_active');
+  }
+  else {
+    console.log('Деактивирую кнопку');
+    btnElement.setAttribute('disabled', true);
+    btnElement.classList.remove('search__button_active');
+  }
+};
+
+function validateInput() {
+  event.preventDefault();
+  if (searchField.value.length > 0) {
+    document.querySelector(".search__error-message").classList.add("search__error-message_hidden");
+    activateFormButton(submitButton, true);
+    document.querySelector("#searchButton").addEventListener('click', search);
+  }
+  else {
+    document.querySelector(".search__error-message").classList.remove("search__error-message_hidden");
+    activateFormButton(submitButton, false);
+  }
+}
+
+function search(event) {
+  submitButton.setAttribute('disabled', true);
   const searchInput = document.querySelector(".search__input").value;
-
   let res = new Api(`https://newsapi.org/v2/everything?q=${searchInput}&apiKey=${apiNews}&pageSize=100&from=${today}&to=${previousWeek}&language=ru`);
-
-
   document.querySelector(".preloader").classList.remove("preloader_hidden");
   document.querySelector(".error").classList.add("error_hidden");
   document.querySelector(".results").classList.add("results_hidden");
+  res.remove;
   res.getApiData()
-
     .then(res => {
       document.querySelector(".preloader").classList.add("preloader_hidden");
       document.querySelector(".results").classList.remove("results_hidden");
       document.querySelectorAll(".result-card").forEach(function (elem) {
         elem.remove();
+        console.log('123');
       })
-      // test(res.articles, searchInput);
-
-
-      new ResultList(document.querySelector(".results__list"), res.articles);
-
+      let newList = new ResultList(document.querySelector(".results__list"), res.articles);
       localStorage.setItem('title', JSON.stringify(searchInput));
-
       const total = res.totalResults;
       localStorage.setItem('total', JSON.stringify(total));
+      document.querySelector("#searchButton").removeEventListener;
+      const titles = res.articles.map((item) => {
+        return item.title
+      });
+      const titleCount = titles.filter((item) => { return item.toLowerCase().includes(`${searchInput}`.toLowerCase()) });
+      localStorage.setItem('titles', JSON.stringify(titleCount.length));
 
-
-      // const dates = articles.map((item) => {
+      // //новая попытка
+      // const dates = titles.map((item) => {
       //   const weekDay = new Date(item.publishedAt);
       //   return weekDay.getDay()
       // });
-      // const sun = dates.filter( (item) => {return item === 0});
+
+      // const sun = dates.filter((item) => { return item === 0 });
       // const sunCount = `${sun.length}`;
+      // // console.log(monCount);
       // localStorage.setItem('sun', JSON.stringify(sunCount));
-      // const mon = dates.filter( (item) => {return item === 1});
+      // const mon = dates.filter((item) => { return item === 1 });
       // const monCount = `${mon.length}`;
       // localStorage.setItem('mon', JSON.stringify(monCount));
-      // const tue = dates.filter( (item) => {return item === 2});
+      // const tue = dates.filter((item) => { return item === 2 });
       // const tueCount = `${tue.length}`;
       // localStorage.setItem('tue', JSON.stringify(tueCount));
-      // const wed = dates.filter( (item) => {return item === 3});
+      // const wed = dates.filter((item) => { return item === 3 });
       // const wedCount = `${wed.length}`;
       // localStorage.setItem('wed', JSON.stringify(wedCount));
-      // const thu = dates.filter( (item) => {return item === 4});
+      // const thu = dates.filter((item) => { return item === 4 });
       // const thuCount = `${thu.length}`;
       // localStorage.setItem('thu', JSON.stringify(thuCount));
-      // const fri = dates.filter( (item) => {return item === 5});
+      // const fri = dates.filter((item) => { return item === 5 });
       // const friCount = `${fri.length}`;
       // localStorage.setItem('fri', JSON.stringify(friCount));
-      // const sat = dates.filter( (item) => {return item === 6});
+      // const sat = dates.filter((item) => { return item === 6 });
       // const satCount = `${sat.length}`;
       // localStorage.setItem('sat', JSON.stringify(satCount));
+      // //Новая попытка
 
-      const titles = res.articles.map((item) => {
-        console.log(item.title);
-          return item.title
-          
-      });
-      const titleCount = titles.filter((item) => {return item.toLowerCase().includes(`${searchInput}`.toLowerCase())});
-      localStorage.setItem('titles', JSON.stringify(titleCount.length));
     })
-
     .catch(res => {
       document.querySelector(".preloader").classList.add("preloader_hidden");
       document.querySelector(".error").classList.remove("error_hidden");
@@ -91,9 +115,7 @@ const search = function (event) {
 //   // localStorage["query"] = JSON.stringify(query);
 //   localStorage.setItem('title', JSON.stringify(query));
 
-
 //   console.log(res.articles);
-
 
 //   const titles = res.articles.map((item) => {
 //     return item.title
@@ -101,13 +123,9 @@ const search = function (event) {
 //   const titleCount = titles.filter((item) => { return item.toLowerCase().includes(`${query}`.toLowerCase()) });
 //   localStorage.setItem('titles', JSON.stringify(titleCount.length));
 
-
-
 //   localStorage["data.length"] = data.length;
 //   // console.log(stats);
 // }
 // JSON.parse(localStorage["stats"]) вызвать на странице analysis.js
 
-
-
-document.querySelector("#searchButton").addEventListener('click', search);
+searchField.addEventListener('input', validateInput);
