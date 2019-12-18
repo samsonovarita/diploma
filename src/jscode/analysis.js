243aac5d0b470c8939cfa7537b22e96928b1beb2
Margarita Samsonova <ritiksamsonova@yandex.ru>
2019-12-18 20:48:22 +0300

    for saving

--- src/jscode/analysis.js
+++ src/jscode/analysis.js
@@ -1,103 +1,98 @@
 import "./../css/analysis.css";
 import { apiNews, today, previousWeek } from "./modules/constants.js";
 
-const month = ['ЯНВАРЬ', 'ФЕВРАЛЬ', 'МАРТ', 'АПРЕЛЬ', 'МАЙ', 'ИЮНЬ', 'ИЮЛЬ', 'АВГУСТ', 'СЕНТЯБРЬ', 'ОКТЯБРЬ', 'НОЯБРЬ', 'ДЕКАБРЬ'];
-
-document.querySelector('.answer__title').textContent = `Вы спросили: «${localStorage.getItem('title').replace(/[\"]/gim, "")}»`
-document.querySelector('.answer__news_total').textContent = localStorage.getItem('total').replace(/[\"]/gim, "");
-document.querySelector('.answer__news_in-title').textContent = localStorage.getItem('titles').replace(/[\"]/gim, "");
-document.querySelector('.diagram__text').textContent = `Дата (${month[new Date().getMonth()]})`;
-
-//новая попытка
 const date = new Date();
-const ago = new Date(date.getTime() - previousWeek);
-const week = arrOfDays(ago);
-const data = JSON.parse(localStorage.getItem('data'));
-console.log(data.articles);
-// Массив дат полученных из API
-const dayOfArticle = data.articles.map((el) => { return el.publishedAt });
-const renderedDate = dayOfArticle.map((el) => { return new Date(el).getDate() });
+const oneDay = 24 * 60 * 60 * 1000;
+const weekAgoTimestamp = 6 * oneDay;
+const ago = new Date(date.getTime() - weekAgoTimestamp);
 
-// Получение массива чисел дней
+// Массив чисел дней
+const weekDays = arrOfDays(ago);
+const days = daysWeek(ago);
+
+const month = ['ЯНВАРЬ', 'ФЕВРАЛЬ', 'МАРТ', 'АПРЕЛЬ', 'МАЙ', 'ИЮНЬ', 'ИЮЛЬ', 'АВГУСТ', 'СЕНТЯБРЬ', 'ОКТЯБРЬ', 'НОЯБРЬ', 'ДЕКАБРЬ'];
+const day = [ 'вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+//получение данных со страницы поиска
+const results = JSON.parse(localStorage.getItem('results'));
+
+//получение массива дат с API из LocalStorage
+const publishedDate = results.articles.map((el) => {return el.publishedAt});
+const renderedDate = publishedDate.map((el) => {return new Date(el).getDate()});
+
+const countTitles = 
+    results.articles.filter(el => {
+        return el.title.toLowerCase().includes(`${localStorage.getItem('title').toLowerCase()}`) }
+    ).length;
+
+// получение массива чисел дней
 function arrOfDays(a) {
-    let arr = [];
+    const arr = [];
     for (let i = 0; i < 7; i += 1) {
-        arr.push(new Date(ago.getTime() + today*i).getDate());
+        arr.push(new Date(ago.getTime() + oneDay*i).getDate());
     }
     return arr
 }
 
-// Количество новостей за каждый из дней
+// получение массива дней недели 
+function daysWeek(a) {
+    const arr = [];
+    for (let i = 0; i < 7; i += 1) {
+        arr.push(new Date(ago.getTime() + oneDay*i).getDay());
+    }
+    return arr
+}
+
+// количество новостей за каждый из дней
 function newsInDay(n) {
     return renderedDate.filter((el) => {
         return el == n
     }).length
 }
 
-// День недели 
+// день недели 
 function dayOfWeek() {
     return days.map((el) => { return day[el] })
 }
 
-// Первый день в списке
-document.querySelector('.table__progress_mon').setAttribute('style', `width: ${newsInDay(week[0])}%`);
-document.querySelector('.table__value_mon').textContent = newsInDay(week[0]);
-document.querySelector('.first-day').textContent = `${week[0]}, ${dayOfWeek()[0]}`;
-
-// Второй день в списке
-document.querySelector('.table__progress_tue').setAttribute('style', `width: ${newsInDay(week[1])}%`);
-document.querySelector('.table__value_tue').textContent = newsInDay(week[1]);
-document.querySelector('.second-day').textContent = `${week[1]}, ${dayOfWeek()[1]}`;
-
-// Третий день в списке
-document.querySelector('.table__progress_wed').setAttribute('style', `width: ${newsInDay(week[2])}%`);
-document.querySelector('.table__value_wed').textContent = newsInDay(week[2]);
-document.querySelector('.third-day').textContent = `${week[2]}, ${dayOfWeek()[2]}`;
-
-// Четвертый день в списке
-document.querySelector('.table__progress_thu').setAttribute('style', `width: ${newsInDay(week[3])}%`);
-document.querySelector('.table__value_thu').textContent = newsInDay(week[3]);
-document.querySelector('.fourth-day').textContent = `${week[3]}, ${dayOfWeek()[3]}`;
-
-// Пятый день в списке
-document.querySelector('.table__progress_fri').setAttribute('style', `width: ${newsInDay(week[4])}%`);
-document.querySelector('.table__value_fri').textContent = newsInDay(week[4]);
-document.querySelector('.fifth-day').textContent = `${week[4]}, ${dayOfWeek()[4]}`;
-
-// Шестой день в списке
-document.querySelector('.table__progress_sat').setAttribute('style', `width: ${newsInDay(week[5])}%`);
-document.querySelector('.table__value_sat').textContent = newsInDay(week[5]);
-document.querySelector('.sixth-day').textContent = `${week[5]}, ${dayOfWeek()[5]}`;
-
-// Седьмой день в списке
-document.querySelector('.table__progress_sun').setAttribute('style', `width: ${newsInDay(week[6])}%`);
-document.querySelector('.table__value_sun').textContent = newsInDay(week[6]);
-document.querySelector('.seventh-day').textContent = `${week[6]}, ${dayOfWeek()[6]}`;
-
-//новая попытка
+//общая информация
+document.querySelector('.answer__title').textContent = `Вы спросили: «${localStorage.getItem('title').replace(/[\"]/gim, "")}»`
+document.querySelector('.answer__news_total').textContent = results.totalResults;
+document.querySelector('.answer__news_in-title').textContent = countTitles;
+document.querySelector('.diagram__text').textContent = `Дата (${month[new Date().getMonth()]})`;
 
 
+// первый день
+document.querySelector('.diagram__bar_one').setAttribute('style', `width: ${newsInDay(weekDays[0])}%`);
+document.querySelector('.diagram__bar_one').textContent = newsInDay(weekDays[0]);
+document.querySelector('.diagram__date_one').textContent = `${weekDays[0]}, ${dayOfWeek()[0]}`;
 
+// второй день
+document.querySelector('.diagram__bar_two').setAttribute('style', `width: ${newsInDay(weekDays[1])}%`);
+document.querySelector('.diagram__bar_two').textContent = newsInDay(weekDays[1]);
+document.querySelector('.diagram__date_two').textContent = `${weekDays[1]}, ${dayOfWeek()[1]}`;
 
+// третий день
+document.querySelector('.diagram__bar_three').setAttribute('style', `width: ${newsInDay(weekDays[2])}%`);
+document.querySelector('.diagram__bar_three').textContent = newsInDay(weekDays[2]);
+document.querySelector('.diagram__date_three').textContent = `${weekDays[2]}, ${dayOfWeek()[2]}`;
 
+// четвертый день
+document.querySelector('.diagram__bar_four').setAttribute('style', `width: ${newsInDay(weekDays[3])}%`);
+document.querySelector('.diagram__bar_four').textContent = newsInDay(weekDays[3]);
+document.querySelector('.diagram__date_four').textContent = `${weekDays[3]}, ${dayOfWeek()[3]}`;
 
-// document.querySelector('.table__progress_mon').setAttribute('style', `width: ${localStorage.getItem('mon').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_mon').textContent = localStorage.getItem('mon').replace(/[\"]/gim, "");
+// пятый день
+document.querySelector('.diagram__bar_five').setAttribute('style', `width: ${newsInDay(weekDays[4])}%`);
+document.querySelector('.diagram__bar_five').textContent = newsInDay(weekDays[4]);
+document.querySelector('.diagram__date_five').textContent = `${weekDays[4]}, ${dayOfWeek()[4]}`;
 
-// document.querySelector('.table__progress_tue').setAttribute('style', `width: ${localStorage.getItem('tue').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_tue').textContent = localStorage.getItem('tue').replace(/[\"]/gim, "");
+// шестой день
+document.querySelector('.diagram__bar_six').setAttribute('style', `width: ${newsInDay(weekDays[5])}%`);
+document.querySelector('.diagram__bar_six').textContent = newsInDay(weekDays[5]);
+document.querySelector('.diagram__date_six').textContent = `${weekDays[5]}, ${dayOfWeek()[5]}`;
 
-// document.querySelector('.table__progress_wed').setAttribute('style', `width: ${localStorage.getItem('wed').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_wed').textContent = localStorage.getItem('wed').replace(/[\"]/gim, "");
-
-// document.querySelector('.table__progress_thu').setAttribute('style', `width: ${localStorage.getItem('thu').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_thu').textContent = localStorage.getItem('thu').replace(/[\"]/gim, "");
-
-// document.querySelector('.table__progress_fri').setAttribute('style', `width: ${localStorage.getItem('fri').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_fri').textContent = localStorage.getItem('fri').replace(/[\"]/gim, "");
-
-// document.querySelector('.table__progress_sat').setAttribute('style', `width: ${localStorage.getItem('sat').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_sat').textContent = localStorage.getItem('sat').replace(/[\"]/gim, "");
-
-// document.querySelector('.table__progress_sun').setAttribute('style', `width: ${localStorage.getItem('sun').replace(/[\"]/gim, "")}%`);
-// document.querySelector('.table__value_sun').textContent = localStorage.getItem('sun').replace(/[\"]/gim, "");
+// седьмой день
+document.querySelector('.diagram__bar_seven').setAttribute('style', `width: ${newsInDay(weekDays[6])}%`);
+document.querySelector('.diagram__bar_seven').textContent = newsInDay(weekDays[6]);
+document.querySelector('.diagram__date_seven').textContent = `${weekDays[6]}, ${dayOfWeek()[6]}`;
--- src/jscode/index.js
+++ src/jscode/index.js
@@ -7,6 +7,13 @@ import { makeFormattedDate } from "./modules/changeDate.js";
 const submitButton = document.querySelector('.search__button');
 const searchField = document.querySelector(".search__input");
 
+function checkLocalStorage () {
+  if (JSON.parse(results) !== null) {
+      searchField.setAttribute('placeholder',JSON.parse(searchInput));
+      new ResultList(document.querySelector(".results__list"), results);
+  }
+}
+
 function activateFormButton(btnElement, state) {
   if (state) {
     console.log('Активирую кнопку');
@@ -40,7 +47,6 @@ function search(event) {
   document.querySelector(".preloader").classList.remove("preloader_hidden");
   document.querySelector(".error").classList.add("error_hidden");
   document.querySelector(".results").classList.add("results_hidden");
-  res.remove;
   res.getApiData()
     .then(res => {
       document.querySelector(".preloader").classList.add("preloader_hidden");
@@ -49,47 +55,11 @@ function search(event) {
         elem.remove();
         console.log('123');
       })
-      let newList = new ResultList(document.querySelector(".results__list"), res.articles);
+      new ResultList(document.querySelector(".results__list"), res.articles);
+      let results = res;
+      localStorage.setItem('results', JSON.stringify(results));
       localStorage.setItem('title', JSON.stringify(searchInput));
-      const total = res.totalResults;
-      localStorage.setItem('total', JSON.stringify(total));
       document.querySelector("#searchButton").removeEventListener;
-      const titles = res.articles.map((item) => {
-        return item.title
-      });
-      const titleCount = titles.filter((item) => { return item.toLowerCase().includes(`${searchInput}`.toLowerCase()) });
-      localStorage.setItem('titles', JSON.stringify(titleCount.length));
-
-      // //новая попытка
-      // const dates = titles.map((item) => {
-      //   const weekDay = new Date(item.publishedAt);
-      //   return weekDay.getDay()
-      // });
-
-      // const sun = dates.filter((item) => { return item === 0 });
-      // const sunCount = `${sun.length}`;
-      // // console.log(monCount);
-      // localStorage.setItem('sun', JSON.stringify(sunCount));
-      // const mon = dates.filter((item) => { return item === 1 });
-      // const monCount = `${mon.length}`;
-      // localStorage.setItem('mon', JSON.stringify(monCount));
-      // const tue = dates.filter((item) => { return item === 2 });
-      // const tueCount = `${tue.length}`;
-      // localStorage.setItem('tue', JSON.stringify(tueCount));
-      // const wed = dates.filter((item) => { return item === 3 });
-      // const wedCount = `${wed.length}`;
-      // localStorage.setItem('wed', JSON.stringify(wedCount));
-      // const thu = dates.filter((item) => { return item === 4 });
-      // const thuCount = `${thu.length}`;
-      // localStorage.setItem('thu', JSON.stringify(thuCount));
-      // const fri = dates.filter((item) => { return item === 5 });
-      // const friCount = `${fri.length}`;
-      // localStorage.setItem('fri', JSON.stringify(friCount));
-      // const sat = dates.filter((item) => { return item === 6 });
-      // const satCount = `${sat.length}`;
-      // localStorage.setItem('sat', JSON.stringify(satCount));
-      // //Новая попытка
-
     })
     .catch(res => {
       document.querySelector(".preloader").classList.add("preloader_hidden");
@@ -97,35 +67,5 @@ function search(event) {
     })
 }
 
-// function test(data, query) {
-//   let stats = {};
-//   // let totalResults
-//   // let data = 
-//   // const query = document.querySelector(".search__input");
-//   // console.log(data);
-//   data.forEach(function (element) {
-//     let date = makeFormattedDate(element.publishedAt);
-//     if (stats[date] == undefined) {
-//       stats[date] = 0;
-//     }
-//     // console.log(query);
-//     stats[date] += element.description.split(query).length - 1;
-//   });
-//   localStorage["stats"] = JSON.stringify(stats);
-//   // localStorage["query"] = JSON.stringify(query);
-//   localStorage.setItem('title', JSON.stringify(query));
-
-//   console.log(res.articles);
-
-//   const titles = res.articles.map((item) => {
-//     return item.title
-//   });
-//   const titleCount = titles.filter((item) => { return item.toLowerCase().includes(`${query}`.toLowerCase()) });
-//   localStorage.setItem('titles', JSON.stringify(titleCount.length));
-
-//   localStorage["data.length"] = data.length;
-//   // console.log(stats);
-// }
-// JSON.parse(localStorage["stats"]) вызвать на странице analysis.js
-
 searchField.addEventListener('input', validateInput);
+checkLocalStorage();
